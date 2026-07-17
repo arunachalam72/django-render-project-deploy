bae13a6c4c3df5f2d3ab5cab7d35e978d2ae1649
arunrascal <arunrascal30@gmail.com>
2026-07-17 16:46:34 +0530

    staticfiles settings.py changed

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -141,19 +141,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
-    BASE_DIR/'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = BASE_DIR/'static'
-
 MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
